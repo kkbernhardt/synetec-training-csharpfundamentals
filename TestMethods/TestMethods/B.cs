d6e4f58588bb2b52101eb3805aeccdd3b48d92d0
unknown <kbernhardt@synetec.co.uk>
2019-01-04 14:16:05 +0000

    add(TCSF-03)add project

---
+++ TestMethods/TestMethods/B.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMethods
+{
+    class B
+    {
+        public static void SM()
+        {
+            Console.WriteLine("Hello from B.SM()");
+        }
+
+        public virtual void VIM()
+        {
+            Console.WriteLine("Hello from b.VIM()");
+        }
+
+        public void NIM()
+        {
+            Console.WriteLine("Hello from b.NIM()");
+        }
+
+    }
+}
---
+++ TestMethods/TestMethods/C.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMethods
+{
+    class C : B
+    {
+        public new static void SM()
+        {
+            Console.WriteLine("Hi from C.SM()");
+        }
+
+        public override void VIM()
+        {
+            Console.WriteLine("Hi from C.VIM()");
+        }
+
+        public new void NIM()
+        {
+            Console.WriteLine("Hi from C.NIM()");
+        }
+
+
+    }
+}
