8f4e38fb24ac56cc459e717159da10e3c03c52c0
unknown <kbernhardt@synetec.co.uk>
2019-01-04 14:29:02 +0000

    (add)TestAttributeObsolete project

---
+++ TestAttributeObsolete/TestAttributeObsolete/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAttributeObsolete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //there is a warning saying it is an obsolete method but it does compile
+            AcousticModem();
+            AcousticModem("updated method");
+            Console.ReadLine();
+        }
+        [Obsolete("Use updated method with parameter instead this", false)]
+        static void AcousticModem()
+        {
+            Console.WriteLine("beep buup baap bzfttfsst %^@~#&&^@CONNECTION LOST");
+        }
+        static void AcousticModem(string name)
+        {
+            Console.WriteLine("beep buup baap bzfttfsst %^@~#&&^@CONNECTION LOST: " + name);
+        }
+    }
+}
