22f1966b0e4239cdfa856b74fe1910a9a1a4731c
unknown <kbernhardt@synetec.co.uk>
2019-01-18 11:48:43 +0000

    (feat)add test project

---
+++ TestDelegate/TestDelegateTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestDelegateTests
+{
+    public class UnitTestToTestDelegate
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
