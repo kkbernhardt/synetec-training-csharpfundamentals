a85f05fcdf3ae5a66eb62868e5b96dcc040e51f5
unknown <kbernhardt@synetec.co.uk>
2019-01-18 11:39:48 +0000

    (feat)add method getting minutes from struct

--- TestTime/TestTimeTests/UnitTest1.cs
+++ TestTime/TestTimeTests/UnitTest1.cs
@@ -1,10 +1,17 @@
 using System;
+using TestTime;
 using Xunit;
 
 namespace TestTimeTests
 {
     public class UnitTestsForTestTime
     {
-        
+        [Fact]
+        public void ShouldGiveBackMinutesValueOfStruct()
+        {
+            Time testTime = new Time(5, 10);
+
+            Assert.Equal(310, testTime.minutes);
+        }
     }
 }
