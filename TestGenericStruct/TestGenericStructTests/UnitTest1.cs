52d3e16d58f23e0ce5c612dca1e6fe7b56c33128
unknown <kbernhardt@synetec.co.uk>
2019-01-18 14:44:18 +0000

    (feat)add testMethod istype

--- TestGenericStruct/TestGenericStructTests/UnitTest1.cs
+++ TestGenericStruct/TestGenericStructTests/UnitTest1.cs
@@ -12,5 +12,12 @@ namespace TestGenericStructTests
             Pair<string, int> pairStruct = new Pair<string, int>("Andrew", 2);
             Assert.Equal("Andrew", pairStruct.Fst);
         }
+
+        [Fact]
+        public void ShouldBeTypeInt()
+        {
+            Pair<string, int> pairStruct = new Pair<string, int>("Andrew", 2);
+            Assert.IsType<int>(pairStruct.Snd);
+        }
     }
 }
