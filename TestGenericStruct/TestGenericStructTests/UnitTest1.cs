f4e2d88723171cddf86a6c76aa0f3d82e8f7d88a
unknown <kbernhardt@synetec.co.uk>
2019-01-18 14:43:21 +0000

    (feat)add method check equal

--- TestGenericStruct/TestGenericStructTests/UnitTest1.cs
+++ TestGenericStruct/TestGenericStructTests/UnitTest1.cs
@@ -1,14 +1,16 @@
 using System;
+using TestGenericStruct;
 using Xunit;
 
 namespace TestGenericStructTests
 {
-    public class UnitTest1
+    public class UnitTestsForTestGenericStructProject
     {
         [Fact]
-        public void Test1()
+        public void ShouldBeEqualToFst()
         {
-
+            Pair<string, int> pairStruct = new Pair<string, int>("Andrew", 2);
+            Assert.Equal("Andrew", pairStruct.Fst);
         }
     }
 }
